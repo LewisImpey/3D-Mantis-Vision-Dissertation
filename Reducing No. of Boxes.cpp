46fd2c38050f54f2c3f40f3705f9577f5bb7880e
LewisImpey <72812002+LewisImpey@users.noreply.github.com>
2022-02-09 22:59:29 +0000

    Update Reducing No. of Boxes.cpp
    able to now only show 1 bounding box of the many made. Dimensions of the box is not correct (problem in corner coordinates)

--- Reducing No. of Boxes.cpp
+++ Reducing No. of Boxes.cpp
@@ -16,13 +16,12 @@ using namespace std;
 
 RNG rng(12345);
 Scalar colour = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-
 Mat detectDifference(Mat ImageFrame1, Mat ImageFrame2)
 {
 	Mat diff;
 
-	resize(ImageFrame1, ImageFrame1, Size(), 0.5, 0.5);
-	resize(ImageFrame2, ImageFrame2, Size(), 0.5, 0.5);
+	//resize(ImageFrame1, ImageFrame1, Size(), 0.5, 0.5);
+	//resize(ImageFrame2, ImageFrame2, Size(), 0.5, 0.5);
 
 	// converting to grayscale
 	cvtColor(ImageFrame1, ImageFrame1, COLOR_BGR2GRAY);
@@ -33,11 +32,12 @@ Mat detectDifference(Mat ImageFrame1, Mat ImageFrame2)
 
 	return diff;
 }
+
 Mat applyDilation(Mat diff)
 {
 	Mat dilatedImage;
 	// applying a blur filter to the image to merge neighboring pixels together. This should allow the bounding boxes to pick up larger areas of pixels.
-	GaussianBlur(diff, diff, Size(7, 7), 2, 2); // I think this should be put before the absdiff() function is run to get a more accurate bounding box (blur before makes it considerably slower)
+	GaussianBlur(diff, diff, Size(15, 15), 2, 2); // the blur seems to have a significant affect on the outcome of the image
 
 	Mat kernal = getStructuringElement(MORPH_ERODE, Size(100, 100));
 	dilate(diff, dilatedImage, kernal);
@@ -45,51 +45,50 @@ Mat applyDilation(Mat diff)
 	return dilatedImage, diff;
 }
 
-void detectContours(Mat& image, Mat& dilatedImage, bool isLeftCamera)
+void detectContours(Mat& image, Mat& dilatedImage)
 {
 	vector<vector<Point> > contours;
 	vector<Vec4i> hierarchy;
 	Mat threshold_output;
-	threshold(dilatedImage, threshold_output, 25, 255, THRESH_BINARY);
+	threshold(dilatedImage, threshold_output, 100, 255, THRESH_BINARY);
 	//Canny(threshold_output, threshold_output, 100, 200);
-	findContours(threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+	findContours(threshold_output, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
 	vector<vector<Point> > contours_poly(contours.size());
 	vector<Rect> boundRect(contours.size());
-	Point2f leftCameraCornerCoordinates;
-	Point2f rightCameraCornerCoordinates;
 
-	int biggestBox;
-	for (size_t i = 0; i < contours.size(); i++)
+	int leftmostBox = 0;
+	int rightmostBox = 0;
+
+	for (int i = 0; i < contours.size(); i++)
 	{
+		Scalar colour = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
 		approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true); // can change epsilon accuracy to change polygonal precision
 		boundRect[i] = boundingRect(Mat(contours_poly[i]));
 
-		if (boundRect[i].area() > boundRect[i - 1].area()) biggestBox = i;
-		//if (isLeftCamera == true) { leftCameraCornerCoordinates.x = boundRect[i].x; leftCameraCornerCoordinates.y = boundRect[i].y; isLeftCamera = false; } // not sure about this true of false thing
-		//else { rightCameraCornerCoordinates.x = boundRect[i].x; rightCameraCornerCoordinates.y = boundRect[i].y; isLeftCamera = true; }                     // there's probably a better way to do it
-	}
-	//Mat image = Mat::zeros(threshold_output.size(), CV_8UC3);
-	Mat drawing = Mat::zeros(threshold_output.size(), CV_8UC3);
+		if (i == 0) { leftmostBox = 0; }
+		else if (boundRect[i].tl().x < boundRect[i - 1].tl().x || boundRect[i].tl().y < boundRect[i - 1].tl().y) leftmostBox = i;
 
-	for (size_t i = 0; i < contours.size(); i++)
+		if (i == 0) { rightmostBox = 0; }
+		else if (boundRect[i].br().y > boundRect[i - 1].br().y || boundRect[i].br().x > boundRect[i - 1].br().x) rightmostBox = i;
+	}
+
+	for (int i = 0; i < contours.size(); i++)
 	{
-		drawContours(drawing, contours_poly, (int)i, 255, 1, 8, vector<Vec4i>(), 0, Point());
+		drawContours(image, contours_poly, (int)i, 255, 1, 8, vector<Vec4i>(), 0, Point());
 		//rectangle(image, boundRect[i].tl(), boundRect[i].br(), 255, 2, 8, 0);
-		rectangle(image, boundRect[biggestBox].tl(), boundRect[biggestBox].br(), 255, 2, 8, 0);
-		/*
-		putText(drawing, to_string(leftCameraCornerCoordinates.x), Point(20, 20), FONT_HERSHEY_DUPLEX, 0.75, colour, 4);
-		putText(drawing, to_string(leftCameraCornerCoordinates.y), Point(20, 40), FONT_HERSHEY_DUPLEX, 0.75, colour, 4);
 
-		putText(drawing, to_string(rightCameraCornerCoordinates.x), Point(20, 60), FONT_HERSHEY_DUPLEX, 0.75, colour, 4);
-		putText(drawing, to_string(rightCameraCornerCoordinates.y), Point(20, 80), FONT_HERSHEY_DUPLEX, 0.75, colour, 4);
+		putText(image, to_string(boundRect[leftmostBox].tl().x), Point(10, 20), FONT_HERSHEY_DUPLEX, 0.75, colour, 1);
+		putText(image, to_string(boundRect[leftmostBox].tl().y), Point(10, 40), FONT_HERSHEY_DUPLEX, 0.75, colour, 1);
 
-		imshow("coords", drawing); // find a better way of doing this (in the main probably) and try to add the coords into the corner of each cameras view
-		*/
-		waitKey(1);
-
+		putText(image, to_string(boundRect[rightmostBox].br().x), Point(10, 80), FONT_HERSHEY_DUPLEX, 0.75, colour, 1, true);
+		putText(image, to_string(boundRect[rightmostBox].br().y), Point(10, 100), FONT_HERSHEY_DUPLEX, 0.75, colour, 1, true);
 	}
-
+	rectangle(image, boundRect[leftmostBox].tl(), boundRect[rightmostBox].br(), colour, 2, 8, 0);
+	circle(image, boundRect[leftmostBox].tl(), 3, Scalar(255, 255, 255), FILLED);
+	circle(image, boundRect[leftmostBox].br(), 3, Scalar(255, 255, 255), FILLED);
+	circle(image, boundRect[rightmostBox].tl(), 3, Scalar(255, 255, 255), FILLED);
+	circle(image, boundRect[rightmostBox].br(), 3, Scalar(255, 255, 255), FILLED);
 }
 
 int main()
@@ -98,37 +97,35 @@ int main()
 	Mat rightDiff, rightDilatedImage;
 
 	// this if trying to use an image
-	//string leftImageSource1 = "leftFrame1.jpg";
-	//string rightImageSource1 = "rightFrame1.jpg";
+	////////////////////////////////////////////////////////////////////////////////////
+	string leftImageSource1 = "leftFrame1.jpg";
+	string rightImageSource1 = "rightFrame1.jpg";
 
-	//string leftImageSource2 = "leftFrame2.jpg";
-	//string rightImageSource2 = "rightFrame2.jpg";
+	string leftImageSource2 = "leftFrame2.jpg";
+	string rightImageSource2 = "rightFrame2.jpg";
 
-	//Mat leftImage1 = imread(leftImageSource1);
-	//Mat rightImage1 = imread(rightImageSource1);
+	Mat leftImage1 = imread(leftImageSource1);
+	Mat rightImage1 = imread(rightImageSource1);
 
-	//Mat leftImage2 = imread(leftImageSource2);
-	//Mat rightImage2 = imread(rightImageSource2);
+	Mat leftImage2 = imread(leftImageSource2);
+	Mat rightImage2 = imread(rightImageSource2);
 
-	//leftDiff = detectDifference(leftImage1, leftImage2);
-	//rightDiff = detectDifference(rightImage1, rightImage2);
+	leftDiff = detectDifference(leftImage1, leftImage2);
+	rightDiff = detectDifference(rightImage1, rightImage2);
 
-	//leftDiff = detectDifference(leftImage1, leftImage2);
-	//rightDiff = detectDifference(rightImage1, rightImage2);
+	leftDilatedImage = applyDilation(leftDiff);
+	rightDilatedImage = applyDilation(rightDiff);
 
-	//leftDilatedImage = applyDilation(leftDiff);
-	//rightDilatedImage = applyDilation(rightDiff);
+	detectContours(leftDiff, leftDilatedImage);
+	detectContours(rightDiff, rightDilatedImage);
 
-	//detectContours(leftDiff, leftDilatedImage, true);
-	//detectContours(rightDiff, rightDilatedImage, false);
-
-	//imshow("left image", leftDiff);
-	//imshow("right image", rightDiff);
-	//waitKey(0);
-
+	imshow("Left Image", leftDiff);
+	imshow("Right Image", rightDiff);
+	waitKey(0);
+	/////////////////////////////////////////////////////////////////////////////////////
 	// this is taking image directly from cameras
 
-	
+	/*
 	VideoCapture leftImageSource(0), rightImageSource(1);
 	while (true)
 	{
@@ -148,5 +145,5 @@ int main()
 		imshow("right dilated image", rightDiff);
 		waitKey(1);
 	}
-	
+	*/
 }
